ee0a80e69aac6253e59fed796a8e81e5593acece
Aymeric Peres <aymeric.peres@etud.univ-jfc.fr>
2024-10-15 17:18:14 +0200

    3e commit

--- src/main/java/org/github/torrano/Main.java
+++ src/main/java/org/github/torrano/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //holaaaaaaa
         //que tal
+        //muy bien
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
